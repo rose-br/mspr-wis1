abd2aef37b76f10f065aeb7a731ff5cbb4ab8af4
rose <r.brochardreigne@gmail.com>
2021-02-02 13:01:58 +0100

    update: footer1

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -2,7 +2,50 @@
 
 <footer>
 
-    FOOTER
+    <section id="description">
+        <div class="container">
+            <h2>Nous contacter </h2>
+           <ul>
+
+               <li>
+                   <span> 16 rue du générale de Gaulle </span>
+                   <span> + 33 6 25 ** 45 98 </span>
+                   <span> mail: contact@mcmbeauty.com </span>
+               </li>
+
+           </ul>
+
+        <section id="footer-nav">
+            <div class="container">
+                <h2>Plan du site</h2>
+
+                <nav id="navbar-footer">
+                    <ul>
+                        <li>
+                            <a href="/html/">Accueil </a>
+                        </li>
+                        <li>
+                            <a href="#"> Qui sommes-nous </a>
+                        </li>
+                        <li>
+                            <a href="#">Les prestations </a>
+                        </li>
+                        <li>
+                            <a href="#">Prestations modèles</a>
+                        </li>
+                        <li>
+                            <a href="#">A propos</a>
+                        </li>
+                        <li>
+                            <a href="#">contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+    </section>
+
+    <div id="span">copyright 2020 - Classe de WIS 1</div>
 
 </footer>
 

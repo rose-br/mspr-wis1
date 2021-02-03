8dd9fd782b25b5e724090b48a04e3576c90a86c2
rose <r.brochardreigne@gmail.com>
2021-02-03 09:48:36 +0100

    update: mini card prestataire

--- index.php
+++ index.php
@@ -7,6 +7,34 @@
 
 <button class="btn-primary">accueil</button>
 
+    <div class="card" style="width: 300px;">
+        <img src="..." class="card-img" alt="...">
+        <div class="card-body">
+            <h3 class="card-title">Nom de l'artiste</h3>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">niveau: professionnel.le </li>
+                <li class="list-group-item">travail  à domicile </li>
+                <li class="list-group-item">depuis 3 ans</li>
+            </ul>
+            <a href="#" class="btn btn-primary"> Voir plus </a>
+        </div>
+    </div>
+
+
 </main>
 
 <?php require_once 'template-parts/footer.php'; ?>
+
+
+<div class="card" style="width: 300px;">
+    <img src="..." class="card-img" alt="...">
+    <div class="card-body">
+        <h3 class="card-title">Nom de l'artiste</h3>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">niveau: professionnel.le </li>
+            <li class="list-group-item">travail  à domicile </li>
+            <li class="list-group-item">depuis 3 ans</li>
+        </ul>
+        <a href="#" class="btn btn-primary"> Voir plus </a>
+    </div>
+</div>

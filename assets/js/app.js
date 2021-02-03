f844be627529fcfccec8ceb727dfe25ebf91a679
Amauryne <amauryne627@gmail.com>
2021-02-03 14:26:23 +0100

    Auto stash before merge of "master" and "origin/master"

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,169 +1,17 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/js/app.js":
+(() => {
 /*!***********************!*\
   !*** ./src/js/app.js ***!
   \***********************/
-/***/ (() => {
 
+})();
 
-
-/***/ }),
-
-/***/ "./src/scss/app.scss":
+(() => {
 /*!***************************!*\
   !*** ./src/scss/app.scss ***!
   \***************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n  ╷\n3 │ border: 4px solid red;\r\n  │                      ^\n  ╵\n  src\\scss\\layout\\_footer.scss 3:22  @import\n  C:\\wamp64\\www\\mspr-wis1\\src\\scss\\app.scss 9:9                          root stylesheet\n    at processResult (C:\\wamp64\\www\\mspr-wis1\\node_modules\\webpack\\lib\\NormalModule.js:597:19)\n    at C:\\wamp64\\www\\mspr-wis1\\node_modules\\webpack\\lib\\NormalModule.js:691:5\n    at C:\\wamp64\\www\\mspr-wis1\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\wamp64\\www\\mspr-wis1\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\wamp64\\www\\mspr-wis1\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass\\sass.dart.js:91024:16)\n    at _render_closure1.call$2 (C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass\\sass.dart.js:79938:12)\n    at _RootZone.runBinary$3$3 (C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass\\sass.dart.js:27187:18)\n    at _FutureListener.handleError$1 (C:\\wamp64\\www\\mspr-wis1\\node_modules\\sass\\sass.dart.js:25715:19)");
+})();
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = __webpack_modules__;
-/******/ 	
-/******/ 	// the startup function
-/******/ 	// It's empty as some runtime module handles the default behavior
-/******/ 	__webpack_require__.x = x => {}
-/************************************************************************/
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/jsonp chunk loading */
-/******/ 	(() => {
-/******/ 		// no baseURI
-/******/ 		
-/******/ 		// object to store loaded and loading chunks
-/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
-/******/ 		// Promise = chunk loading, 0 = chunk loaded
-/******/ 		var installedChunks = {
-/******/ 			"/js/app": 0
-/******/ 		};
-/******/ 		
-/******/ 		var deferredModules = [
-/******/ 			["./src/js/app.js"],
-/******/ 			["./src/scss/app.scss"]
-/******/ 		];
-/******/ 		// no chunk on demand loading
-/******/ 		
-/******/ 		// no prefetching
-/******/ 		
-/******/ 		// no preloaded
-/******/ 		
-/******/ 		// no HMR
-/******/ 		
-/******/ 		// no HMR manifest
-/******/ 		
-/******/ 		var checkDeferredModules = x => {};
-/******/ 		
-/******/ 		// install a JSONP callback for chunk loading
-/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
-/******/ 			var [chunkIds, moreModules, runtime, executeModules] = data;
-/******/ 			// add "moreModules" to the modules object,
-/******/ 			// then flag all "chunkIds" as loaded and fire callback
-/******/ 			var moduleId, chunkId, i = 0, resolves = [];
-/******/ 			for(;i < chunkIds.length; i++) {
-/******/ 				chunkId = chunkIds[i];
-/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
-/******/ 					resolves.push(installedChunks[chunkId][0]);
-/******/ 				}
-/******/ 				installedChunks[chunkId] = 0;
-/******/ 			}
-/******/ 			for(moduleId in moreModules) {
-/******/ 				if(__webpack_require__.o(moreModules, moduleId)) {
-/******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
-/******/ 				}
-/******/ 			}
-/******/ 			if(runtime) runtime(__webpack_require__);
-/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
-/******/ 			while(resolves.length) {
-/******/ 				resolves.shift()();
-/******/ 			}
-/******/ 		
-/******/ 			// add entry modules from loaded chunk to deferred list
-/******/ 			if(executeModules) deferredModules.push.apply(deferredModules, executeModules);
-/******/ 		
-/******/ 			// run deferred modules when all chunks ready
-/******/ 			return checkDeferredModules();
-/******/ 		}
-/******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
-/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
-/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 		
-/******/ 		function checkDeferredModulesImpl() {
-/******/ 			var result;
-/******/ 			for(var i = 0; i < deferredModules.length; i++) {
-/******/ 				var deferredModule = deferredModules[i];
-/******/ 				var fulfilled = true;
-/******/ 				for(var j = 1; j < deferredModule.length; j++) {
-/******/ 					var depId = deferredModule[j];
-/******/ 					if(installedChunks[depId] !== 0) fulfilled = false;
-/******/ 				}
-/******/ 				if(fulfilled) {
-/******/ 					deferredModules.splice(i--, 1);
-/******/ 					result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
-/******/ 				}
-/******/ 			}
-/******/ 			if(deferredModules.length === 0) {
-/******/ 				__webpack_require__.x();
-/******/ 				__webpack_require__.x = x => {};
-/******/ 			}
-/******/ 			return result;
-/******/ 		}
-/******/ 		var startup = __webpack_require__.x;
-/******/ 		__webpack_require__.x = () => {
-/******/ 			// reset startup function so it can be called again when more startup code is added
-/******/ 			__webpack_require__.x = startup || (x => {});
-/******/ 			return (checkDeferredModules = checkDeferredModulesImpl)();
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	// run startup
-/******/ 	return __webpack_require__.x();
 /******/ })()
 ;
--- index.php
+++ index.php
@@ -4,13 +4,89 @@
 <hr
 <hr
 
-<main>
+    <main>
+        <section id="main">
+            <section class="container">
+                <img src="assets/images/image-home.jpg" class="img-top" alt="...">
 
+<<<<<<< Updated upstream
     HOME
 
     <button class="btn-primary">accueil</button>
+=======
+                <h5 class="title-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+                <p class="text-home">TLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan nibh eu ante
+                    egestas,
+                    id
+                    placerat est pellentesque. Donec sed augue quam. Nam varius sapien nibh, quis euismod massa
+                    tincidunt sit amet. Fusce facilisis sit amet neque et ornare. Integer imperdiet ligula a
+                    ligula
+                    pellentesque, ac tincidunt massa egestas. Phasellus blandit mollis erat, id molestie nunc
+                    pharetra non. Nullam imperdiet tempor mauris sed suscipit. Phasellus non pretium augue.
+                    Phasellus augue neque, tincidunt ac ornare sed, accumsan in elit. Aenean mi risus, tempus in
+                    neque efficitur, sagittis pharetra felis. Cras in dui tellus. Maecenas in viverra justo, ac
+                    pellentesque nibh. In tincidunt, odio a maximus finibus, dui dui faucibus est, quis viverra
+                    orci
+                    nulla nec sem. Fusce non elit nisi. Vivamus ut sem rutrum, placerat urna sit amet, facilisis
+                    purus.
+
+                    Mauris consectetur lacus non ante rhoncus fermentum. Vestibulum sollicitudin ultricies
+                    lectus,
+                    nec porta orci mattis vel. Suspendisse non euismod nisi. Suspendisse luctus nulla ac mattis
+                    feugiat. Sed efficitur lobortis orci, sed sollicitudin metus ultricies sit amet. Sed
+                    placerat mi
+                    nunc. Suspendisse sollicitudin ipsum et commodo convallis. Sed lobortis fermentum mauris
+                    consequat pretium. Pellentesque ultricies sapien eget libero luctus viverra. Curabitur diam
+                    velit, aliquam nec quam ac, euismod congue elit. Morbi imperdiet quis libero nec tincidunt.
+                    Curabitur bibendum ligula pellentesque malesuada posuere. Duis lobortis nec magna eget
+                    dapibus.
+
+                    Donec consequat, erat vel ornare sodales, massa diam iaculis risus, eget tempor turpis dolor
+                    a
+                    nulla. Vestibulum eleifend ultrices leo, sit amet pretium sem. Ut feugiat est non metus
+                    interdum
+                    auctor. Integer fringilla lorem id sapien dignissim vehicula eget eu nulla. Duis et cursus
+                    dolor, sit amet mollis ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                    In
+                    hac habitasse platea dictumst.
 
 </main>
+
+    <section id="name">
+        <div class="card bg-dark text-white">
+            <img src="assets/images/coiffure-home.jpg" class="card-img" alt="Coiffure">
+            <div class="card-img-overlay">
+                <h5 class="card-title">Coiffure</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.</p>
+                <p class="card-text">Last updated 3 mins ago</p>
+                <button type="button" class="btn btn-outline-light">Découvrir</button>
+            </div>
+        </div>
+        <div class="card bg-dark text-white">
+            <img src="assets/images/makeup-home.jpg" class="card-img" alt="Coiffure">
+            <div class="card-img-overlay">
+                <h5 class="card-title">Maquillage</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.</p>
+                <p class="card-text">Last updated 3 mins ago</p>
+                <button type="button" class="btn btn-outline-light">Découvrir</button>
+            </div>
+        </div>
+        <div class="card bg-dark text-white">
+            <img src="assets/images/manucure-home.jpg" class="card-img" alt="Coiffure">
+            <div class="card-img-overlay">
+                <h5 class="card-title">Manucure</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.</p>
+                <p class="card-text">Last updated 3 mins ago</p>
+                <button type="button" class="btn btn-outline-light">Découvrir</button>
+            </div>
+        </div>
+    </section>
+    </section>
+>>>>>>> Stashed changes
+
 
 <?php require_once 'template-parts/footer.php'; ?>
 

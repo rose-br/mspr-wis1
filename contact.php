81e6a0be81aa5e8214c63c4783f21a49aa4e6687
rose <r.brochardreigne@gmail.com>
2021-02-23 21:12:39 +0100

    contact map

--- contact.php
+++ contact.php
@@ -1,54 +1,45 @@
-<?php require_once 'template-parts/header.php'; ?>
-<body style="background-image: url(assets/images/back-g.png)">
+    <?php require_once 'template-parts/header.php'; ?>
+    <body style="background-image: url(assets/images/back-g.png)">
 
 
-    <div class="container">
-       <h1> Nous contacter </h1>
+        <div class="container">
+           <h1> Nous contacter </h1>
 
-        <ul> 16 rue du générale de Gaulle</ul>
-        <ul> 44200 Nantes </ul>
-        <ul> mail: contact@mcmbeauty.com</ul>
-        <ul>+ 33 6 25 24 45 98 </ul>
-    </div>
+            <ul> 16 rue du générale de Gaulle</ul>
+            <ul> 44200 Nantes </ul>
+            <ul> mail: contact@mcmbeauty.com</ul>
+            <ul>+ 33 6 25 24 45 98 </ul>
+        </div>
 
-    <div class="text-center">
-        <h2> Rose Brochard-Reigné</h2>
-        <ul> mon linkedin</ul>
-        <ul> mon site CV</ul>
-        <button type="button" class=" btn btn-primary btn-lg "> Bouton pour télécharger mon CV papier</button>
-    </div>
+       <div class="container">
+           <div class="row align-items-start">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d86684.4477474749!2d-1.6300955507595754!3d47.23831721316538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805ee81f0a8aead%3A0x40d37521e0ded30!2sNantes!5e0!3m2!1sfr!2sfr!4v1614109076564!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+       </div>
 
-
-    <div class="text-center">
-        <h2> Amauryne Duval</h2>
-        <ul> mon linkedin</ul>
-        <ul> mon site CV</ul>
-        <button type="button" class=" btn btn-primary btn-lg "> Bouton pour télécharger mon CV papier</button>
-    </div>
-
-<section id="contact">
-    <div class="container">
-        <div class="wrapper">
-            <div class="card">
-                <img src="assets/images/makeup-home.jpg"/>
-                <div class="info">
-                    <h1>Amauryne Duval</h1>
-                    <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
-                    <button>Télecharger CV</button>
+        <section id="contact">
+        <div class="container">
+            <div class="wrapper">
+                <div class="card">
+                    <img src="assets/images/makeup-home.jpg"/>
+                    <div class="info">
+                        <h1>Amauryne Duval</h1>
+                        <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
+                        <button>Télecharger CV</button>
+                    </div>
                 </div>
-            </div>
-            <div class="card">
-                <img src="assets/images/coiffure-home.jpg"/>
-                <div class="info">
-                    <h1>Rose Brochard-Reigné</h1>
-                    <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
-                    <button>Télecharcher CV</button>
+                <div class="card">
+                    <img src="assets/images/coiffure-home.jpg"/>
+                    <div class="info">
+                        <h1>Rose Brochard-Reigné</h1>
+                        <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
+                        <button>Télecharcher CV</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</section>
-</body>
+    </section>
+    </body>
 
-<?php require_once 'template-parts/footer.php'; ?>
+    <?php require_once 'template-parts/footer.php'; ?>

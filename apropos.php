09d39e472f85464a4f68ed64138cfcba6a085c40
Amauryne <amauryne627@gmail.com>
2021-02-23 19:53:28 +0100

    level up 11

--- apropos.php
+++ apropos.php
@@ -1,29 +1,30 @@
-    <?php require_once 'template-parts/header.php'; ?>
-    </body>
-    <section>
+<?php require_once 'template-parts/header.php'; ?>
 
-        <section id="apropos">
-            <div class="container position-absolute top-50 start-50 translate-middle"  >
+<main id="main">
 
-                <H2> Bonjour à tous ! </H2>
-                <p> Notre entreprise a pour objectif de référencer les professionnels et prestataires de maquillage,coiffure et manucure.
+    <section id="qsn">
+        <section id="home-hero" style="background-image: url(assets/images/tache-violet2.png)" >
+            <div class="container">
 
-                    Le but de notre entreprise est d'augmenter la visibilité de ses indépendants en exposant leurs modèles photos dans les milieux qu’ils pratiquent. Elle permettra aussi de faciliter l’entrée dans la vie active de ce milieu pour les apprenties encore en formation  qui souhaite déjà commencer à exercer et accroître leur visibilité pour un éventuel futur emploi professionnel. Notre entreprise permettra aussi à ses apprenties de trouver des modèles pour leurs évaluations de compétence dans leurs écoles.
-
-                    Ce formulaire qui propose de justement devenir modèle pour ses apprenties pourra être sur des client qui souhaite simplement aider les apprenties ou même bien des professionnels pour tester leurs compétences et même bien leurs proposer des stage ou d’autres offres professionnelles. Ce formulaire pourra permettre aux inscrits de recevoir des offres de prestation avec le lieu, la date et toutes les infos supplémentaires pour satisfaire l'attente du prestataire et du modèle.
-
-                    Nous nous engageons également a certifié tous les profils de notre site internet avec une équipe qui valide les compétences, les services et prix de chaque prestataire visible sur notre site web. Notre équipe se constitue de professionnels de la coiffure, de la manucure et du maquillage.
+                <h1 class="display-3">Bonjour à tous ! </h1>
+                <p class="lead">Notre entreprise a pour objectif de référencer les professionnels et prestataires de maquillage,coiffure et manucure.
+                    Le but de notre entreprise est d'augmenter la visibilité de ses indépendants en exposant leurs modèles photos dans les milieux qu’ils pratiquent.
+                    Elle permettra aussi de faciliter l’entrée dans la vie active de ce milieu pour les apprenties encore en formation qui souhaite déjà commencer à exercer et
+                    accroître leur visibilité pour un éventuel futur emploi professionnel.
+                    Notre entreprise permettra aussi à ses apprenties de trouver des modèles pour leurs évaluations de compétence dans leurs écoles.
+                    Ce formulaire qui propose de justement devenir modèle pour ses apprenties pourra être sur des client qui souhaite simplement aider les apprenties
+                    ou même bien des professionnels pour tester leurs compétences et même bien leurs proposer des stage ou d’autres offres professionnelles.
+                    Ce formulaire pourra permettre aux inscrits de recevoir des offres de prestation avec le lieu, la date et toutes les infos supplémentaires
+                    pour satisfaire l'attente du prestataire et du modèle. Nous nous engageons également a certifié tous
+                    les profils de notre site internet avec une équipe qui valide les compétences, les services et prix de chaque prestataire visible sur notre site web.
+                    Notre équipe se constitue de professionnels de la coiffure, de la manucure et du maquillage.</p>
 
 
             </div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109371.39305638435!2d-1.6300955507595454!3d47.23831721316538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805ee81f0a8aead%3A0x40d37521e0ded30!2sNantes!5e1!3m2!1sfr!2sfr!4v1614010328767!5m2!1sfr!2sfr"
-                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="position-absolute top-100 start-50 translate-middle"></iframe>
+
         </section>
-
-
-
-
-
     </section>
-    </body>
+
+</main>
+<?php require_once 'template-parts/footer.php'; ?>
 
